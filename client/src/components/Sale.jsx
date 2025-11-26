769aa4e4d8f41f927aefa6fd6ec58e113710a760
mdTarek-web <mdtarek2330749@gmail.com>
2025-11-26 18:14:40 +0600

    Update Sale.jsx

--- client/src/components/Sale.jsx
+++ client/src/components/Sale.jsx
@@ -9,7 +9,7 @@ const Sale = () => {
             <img src={saleImgOne} alt="saleImgOne" className='w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out'/>
             <div className='absolute w-full h-full top-0 left-0 bg-black/40 text-white/80 flex items-center md:justify-center'>
                 <div className='flex flex-col md:items-center gap-2 p-10'>
-                    <p className='text-sm md:text-lg font-medium text-white'>10% sale ongoing on phone</p>
+                    <p className='text-sm md:text-lg font-medium text-white'>10% sale ongoing on Furniture</p>
                     <p className='text-sm md:text-xl font-semibold'>Offers on limited time</p>
                     <Link to="/shop" className='bg-white/70 text-black px-6 py-2.5 rounded-md hover:bg-white duration-300 font-medium'>Shop Now</Link>
                 </div>
@@ -20,7 +20,7 @@ const Sale = () => {
                 <img src={saleImgTwo} alt="saleImgTwo" className='w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out'/>
                 <div className='absolute w-full h-full top-0 left-0 bg-black/40 text-white/80 flex items-center'>
                   <div className='flex flex-col items-start gap-2 p-10'>
-                    <p className='text-sm md:text-sm font-medium text-white'>10% sale ongoing on phone</p>
+                    <p className='text-sm md:text-sm font-medium text-white'>10% sale ongoing on Furniture</p>
                     <p className='text-sm md:text-lg font-semibold'>Offers on limited time</p>
                     <Link to="/shop" className='bg-white/70 text-black px-6 py-2.5 rounded-md hover:bg-white duration-300 font-medium'>Shop Now</Link>
                   </div>
@@ -30,7 +30,7 @@ const Sale = () => {
                 <img src={saleImgThree} alt="saleImgThree" className='w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out'/>
                 <div className='absolute w-full h-full top-0 left-0 bg-black/40 text-white/80 flex items-center '>
                   <div className='flex flex-col items-start gap-2 p-10'>
-                    <p className='text-sm md:text-sm font-medium text-white'>10% sale ongoing on phone</p>
+                    <p className='text-sm md:text-sm font-medium text-white'>10% sale ongoing on Headphone</p>
                     <p className='text-sm md:text-lg font-semibold'>Offers on limited time</p>
                     <Link to="/shop" className='bg-white/70 text-black px-6 py-2.5 rounded-md hover:bg-white duration-300 font-medium'>Shop Now</Link>
                  </div>
